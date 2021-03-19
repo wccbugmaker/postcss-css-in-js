0f246b23ffb43bda62aec2b7a6e85d82d26c86d9
Ilya Chudin <chudin.ilya@gmail.com>
2021-03-19 10:37:41 +0400

    enable no-confusing-arrow

--- un-camel-case.js
+++ un-camel-case.js
@@ -1,13 +1,13 @@
 'use strict';
 
 function unCamelCase(str) {
-	return str.replace(/[\w-]+/g, (s) =>
-		/^[A-Z]?[a-z]*(?:[A-Z][a-z]*)+$/.test(s)
+	return str.replace(/[\w-]+/g, (s) => {
+		return /^[A-Z]?[a-z]*(?:[A-Z][a-z]*)+$/.test(s)
 			? s
 					.replace(/[A-Z]/g, (casedStr) => `-${casedStr.toLowerCase()}`)
 					.replace(/^(o|ms|moz|khtml|epub|(\w+-?)*webkit)(?=-)/i, '-$1')
-			: s,
-	);
+			: s;
+	});
 }
 
 module.exports = unCamelCase;
